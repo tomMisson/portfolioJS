88cbb8666692766d79b3e86ca30bb3cbcb684335
Thomas Misson <11tmisson@gmail.com>
2019-04-19 14:34:10 +0100

    create default connection to mongo and intitalise if not already exisit

--- Backend/db/connection.js
+++ Backend/db/connection.js
@@ -1,17 +1,9 @@
 let MongoClient = require('mongodb').MongoClient;
 
-
 let db = MongoClient.connect("mongodb://localhost:27017/e-portfolio", { useNewUrlParser: true },  function(err, db) {
     
     if(!err) {
       console.log("We are connected");
     }
     let dbo = db.db("E-portfolio");
-    let Qualifications = dbo.createCollection("Qualifications");
-    console.log("Qualifications collection created!");
-    let Projects = dbo.createCollection("Projects");
-    console.log("Projects collection created!");
-
-    db.close();
-
 });
