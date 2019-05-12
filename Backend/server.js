cfbb84e71a4b50a005283f38c6cd5c1fc3959e94
Thomas Misson <11tmisson@gmail.com>
2019-05-12 18:55:04 +0100

    working codebase

--- Backend/server.js
+++ Backend/server.js
@@ -1,16 +1,42 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+const axios = require('axios');
 const path = require('path');
 const app = express();
 
 app.use(express.static(path.join('../Frontend/', 'build')));
 
-app.get('/ping', function (req, res) {
- return res.send('pong');
+app.get('/api/git-projects', async function (req, res) {
+  let response = await axios.get('https://api.github.com/users/tomMisson/repos').catch();
+    let data = response.data;
+
+    let o = {};
+    o['Projects'] = [];
+
+    for(let i=0; i<data.length; i++)
+    {
+        o['Projects'].push(
+        project = {
+            "id": i,
+            "name": data[i].name,
+            "url": data[i].html_url,
+            "desc": data[i].description,
+            "lang": data[i].language,
+            "forks": data[i].forks,
+            "stars": data[i].stargazers_count
+        });
+    }
+
+  res.json(o['Projects']);
 });
 
-app.get('/', function (req, res) {
+app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(process.env.PORT || 80);
+app.listen(process.env.PORT || 3001);
+
+async function tableData()
+{
+    
+    return o['Projects'];
+}
--- Frontend/src/Components/Pages/Code.js
+++ Frontend/src/Components/Pages/Code.js
@@ -3,15 +3,41 @@ import React, { Component } from 'react'
 export default class Code extends Component {
 
   state = {
-    projects:[]
-
+      projects:[]
   }
-  
+
+  componentDidMount(){
+    fetch('/api/git-projects')
+    .then(res => res.json(res))
+    .then(project => this.setState({ projects: project}))
+  }
 
   render() {
     return (
         <main>
             <h1>Code</h1>
+            <table>
+              <thead>
+                <tr>
+                  <th>Name</th>
+                  <th>Description</th>
+                  <th>Language</th>
+                  <th>Forks</th>
+                  <th>Stars</th>
+                </tr>
+              </thead>
+              <tbody>
+              {this.state.projects.map(project =>
+                <tr key={project.id}>
+                  <td><a alt="project link" href={project.url}>{project.name}</a></td>
+                  <td>{project.desc}</td>
+                  <td>{project.lang}</td>
+                  <td>{project.forks}</td>
+                  <td>{project.stars}</td>
+                </tr>
+              )}
+              </tbody>
+            </table>
         </main>
     )
   }
