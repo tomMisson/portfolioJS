91fa1b55cc2c1c75618895c66b8e53433cd1dd04
Thomas Misson <11tmisson@gmail.com>
2019-09-30 13:45:41 +0100

    added build file

---
+++ Project/build/precache-manifest.f0dc2e3831522a742391f6b713377f84.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "527fd70f4cf948cae387f7b6a83b35dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "74d3768f80a2dca878ed",
+    "url": "/static/css/main.34063a63.chunk.css"
+  },
+  {
+    "revision": "29b96657d07df17117de",
+    "url": "/static/js/2.6faba9b3.chunk.js"
+  },
+  {
+    "revision": "74d3768f80a2dca878ed",
+    "url": "/static/js/main.f6ae6d17.chunk.js"
+  },
+  {
+    "revision": "31d3ee3571a77ec13bf7",
+    "url": "/static/js/runtime-main.64f49c71.js"
+  },
+  {
+    "revision": "430d70088ceb62ed6cffd60929ef4d2b",
+    "url": "/static/media/Background.430d7008.jpg"
+  },
+  {
+    "revision": "fb9a57c5847ff2506b98d675b23ea149",
+    "url": "/static/media/profile.fb9a57c5.jpg"
+  }
+]);
