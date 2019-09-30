373463762532a02655907af105702710f41ba521
Thomas Misson <11tmisson@gmail.com>
2019-09-30 13:08:16 +0100

    Started services page

--- Project/src/Components/Pages/Services.js
+++ Project/src/Components/Pages/Services.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react'
 
 export default class Services extends Component {
+    
+    state={
+        serviceList:[
+            {name:"Web hosting",details:""},
+            {name:"Bespoke software",details:""},
+        ]
+    }
+
+    
     render() {
         return (
             <main>
                 <h1>Services</h1>
+                {this.state.serviceList.map((service) =>
+
+                    <section>
+                        <h2>{service.name}</h2>
+                        <p>{service.details}</p>
+                    </section>
+                    
+                )}
+
             </main>
         )
     }
