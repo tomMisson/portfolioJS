121509f5c8243bb42e6cf7dd0d6d2167e1131e47
tomMisson <11tmisson@gmail.com>
2020-05-12 23:20:47 +0100

    Restructure and remove old content

--- src/App.js
+++ src/App.js
@@ -1,34 +1,25 @@
 import React from 'react';
 import './App.css';
-
-import Email from './Media/Social/email.png';
-import Github from './Media/Social/github.png';
-import LinkedIn from "./Media/Social/linkedin.png";
-import Twitter from './Media/Social/twitter.png';
-import Snapchat from "./Media/Social/snapchat.png";
-import Instagram from "./Media/Social/instagram.png";
-import Home from './Components/Home';
+import Jumbotron from './Jumbotron';
+import JumboBG from './Media/Jumbotron/Background.jpg';
+import ProfilePic from './Media/Jumbotron/profile.jpg';
 
 
 class App extends React.Component {
 
-    state = {
-        externallinks: [
-            { id: 1, pageName: "Email", directory: "mailto:hi@tommisson.uk", image: Email },
-            { id: 2, pageName: "Github", directory: "https://github.com/tomMisson", image: Github },
-            { id: 3, pageName: "LinkedIn", directory: "https://www.linkedin.com/in/thomasmisson/", image: LinkedIn },
-            { id: 4, pageName: "Twitter", directory: "https://twitter.com/thomasmisson", image: Twitter },
-            { id: 5, pageName: "Snapchat", directory: "https://www.snapchat.com/add/tommisson", image: Snapchat },
-            { id: 6, pageName: "Instagram", directory: "https://www.instagram.com/tommisson/", image: Instagram },
-        ]
+    state={
+        jumbotron:{
+        background:JumboBG,
+        avatar:ProfilePic,
+        tagline: "💻 Software Dev • ✍️ Blogger • 🏆 @twilio Champion • 👋 @helloHackerSoc Vice-Chair • 🧰 Toolkit manager @Hackathons_UK • 😍 Emoji enthusiast"
+        }
     }
-
 
     render(){
         return ( 
-            <div className="App">
-                <Home/>
-            </div>
+            <main>
+                <Jumbotron information={this.state.jumbotron}/>
+            </main>
         );
     }
 }
--- src/Jumbotron.js
+++ src/Jumbotron.js
@@ -19,12 +19,8 @@ export default class Jumbotron extends React.Component {
                 <div className="avatar">
                     <img src={this.props.information.avatar} alt="Avatar" width="250" height="250" className = "avatar-position" style={{borderRadius:"50%", boxShadow: "1px 0px 5px rgba(0,0,0,0.5)"}}/>
                 </div>
-                <h3 style={{textShadow: "2px 0px 2px rgba(0,0,0,0.5)"}}>{this.props.information.tagline}
-                <div className="read-more-scroll">
-                <br></br>
-                    <button id="scrollBtn" onClick = {this.scrollToDiv}>&#x21a1;Read More&#x21a1;</button>   
-                </div>
-                </h3>
+                <h3 style={{textShadow: "2px 0px 2px rgba(0,0,0,0.5)"}}>{this.props.information.tagline}</h3>
+                <a href="https://docs.google.com/document/d/1Kqr4zAkT3qddC5qceM1zHs8fDvMlOnNuGzAasCxdM9c/export?format=pdf" download="TomMisson_CV.pdf"><button className="" type="submit">Hire me!</button></a>
             </div>    
         </div>
     )}
